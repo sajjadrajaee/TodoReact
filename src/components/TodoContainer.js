fb84441fefd338a5b8cc988ea70c53ee0e17b1d5
Sajad Rajaee <sajjadrajaee@gmail.com>
2022-11-01 17:00:36 +0330

    add basic components

---
+++ src/components/TodoContainer.js
@@ -0,0 +1,54 @@
+import React from "react";
+import TodoList from "./TodoList";
+import Header from "./Header";
+
+class TodoContainer extends React.Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: "Setup development environment",
+        completed: true
+      },
+      {
+        id: 2,
+        title: "Develop website and add content",
+        completed: false
+      },
+      {
+        id: 3,
+        title: "Deploy to live server",
+        completed: false
+      }
+    ]
+  };
+
+  handleChange = (id) => {
+    this.setState(prevState => ({
+      todos: prevState.todos.map(todo => {
+        if(todo.id === id) {
+          return{
+            ...todo,
+            completed: !todo.completed,
+          }
+        }
+        return todo;
+      })
+    }))
+  }
+
+  delTodo = (id) => {
+    console.log("delete", id);
+  }
+
+  render() {
+    return (
+      <>
+        <Header />
+        <TodoList todos={this.state.todos} handleChangeProps={this.handleChange} deleteTodoProps={this.delTodo}/>
+      </>
+    );
+  }
+}
+
+export default TodoContainer;
